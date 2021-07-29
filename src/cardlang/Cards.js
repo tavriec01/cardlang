580c03425c2d9697160293342fe1946af0bd9f75
tavriec01 <osaliv@mail.ru>
2021-07-29 17:07:25 +0300

    Working with input

--- src/cardlang/Cards.js
+++ src/cardlang/Cards.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 export default function Cards() {
   return (
-    <div className="card_table">
-      <div className="card plus"></div>
+    <div className="cardpage">
+        <div className="card_table">
+            <div className="card plus"></div>
+        </div>
+        <div className="form">
+            <form action="">
+                <div className="input">
+                    URL:
+                    <input type="text" />
+                </div>
+                <div className="input">
+                    Word:
+                    <input type="text" />
+                </div>
+                <div className="input">
+                    <button>Add</button>
+                </div>
+            </form>
+        </div>
     </div>
-    
   );
 }

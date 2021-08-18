969e41553d3928714480504db7e285cc7fce8302
tavriec01 <osaliv@mail.ru>
2021-08-18 15:51:23 +0300

    Now you can add new words, but without image(

--- src/cardlang/Cards.js
+++ src/cardlang/Cards.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function Cards() {
-  return (
-    <div className="cardpage">
-        <div className="card_table">
-            <div className="card plus"></div>
-        </div>
-        <div className="form">
-            <form action="">
-                <div className="input">
-                    URL:
-                    <input type="text" />
+export default class Cards extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            input: '',
+            items: []
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({
+            input: this.state.input,
+            items: [...this.state.items, this.state.input]
+        });
+    }
+
+    render() {
+
+        return (
+            <div className="cardpage">
+                <div className="form">
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="input">
+                            URL:
+                            <input/>
+                        </div>
+                        <div className="input">
+                            Word:
+                            <input value={this.state.input} onChange={this.handleChange}/>
+                        </div>
+                        <div className="input">
+                            <button type="submit">Add</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="input">
-                    Word:
-                    <input type="text" />
+                <div className="card_table">
+                    <div className="card">
+                        <p>Type your image URL and Word, then click on button Add</p> 
+                    </div>                    
+                    {this.state.items.map((item, index) => (
+                        <div className="card" key={index}> <p>{item}</p> </div>
+                    ))}
                 </div>
-                <div className="input">
-                    <button>Add</button>
-                </div>
-            </form>
-        </div>
-    </div>
-  );
+            </div>
+        );
+    }
 }
